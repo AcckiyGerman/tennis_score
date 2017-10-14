4b91d70fcfcdf34a0620754c6f863b3db7095ab4
Dmytro Gierman <mryakzp@gmail.com>
2017-10-14 17:51:04 +0300

    test case for game.status

--- tennis_tests.py
+++ tennis_tests.py
@@ -6,6 +6,15 @@ class TestTennisGame(unittest.TestCase):
     def testScoresList(self):
         self.assertEqual(Game.SCORES, ['love', 'fifteen', 'thirty', 'advantage', 'deuce'])
 
+    def testStatus(self):
+        table = {(0, 0): 'love all',
+                 (1, 0): 'fifteen love',
+                 (2, 0): 'thirty love',
+                 (3, 0): 'advantage player 1',
+                 (4, 0): 'win player 1'}
+        for scores, status in table.items():
+            self.assertEqual(Game.status(scores[0], scores[1]), status)
+
 
 if __name__ == "__main__":
     unittest.main()
